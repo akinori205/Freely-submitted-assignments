de6c3844d22a7b75c64ea774fa99de3b00b30240
still <akinorihirase@gmail.com>
2024-02-27 23:35:00 +0900

    update two files

--- git.bat
+++ git.bat
@@ -3,7 +3,7 @@ setlocal
 set /p comment=please add comments:
 cd "C:\Users\hirase naoki\python-git"
 git add .
-git commit -m %comment%
-git push -u origin main
+rem git commit -m %comment%
+rem git push -u origin main
 pause
 exit /b 0
